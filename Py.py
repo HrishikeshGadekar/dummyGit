30a91e297abc52b08b9eed147b9d68ad0245b3fe
Hrishikesh Gadekar <55317394+HrishikeshGadekar@users.noreply.github.com>
2025-01-23 11:52:33 +0530

    more conditionals

--- Py.py
+++ Py.py
@@ -4,6 +4,7 @@ if int(input("Enter 1st number: ")) > 10 and int(input("Enter 2nd number: ")) > 
 else:
     print("Both < 10")
 
+# ---
 a, b, c = int(input("Enter 1st number: ")), int(input("Enter 2nd number: ")), int(input("Enter 3rd number: "))
 
 if a >= b and a >= c:
@@ -12,3 +13,17 @@ elif b >= a and b >= c:
     print(f"b = {b} is the greatest.")
 else:
     print(f"c = {c} is the greatest.")
+
+# ---
+l = [67, 12, 8, 6, 3, 2, 0, -2, -23]
+
+for n in l:
+    print(f"n = {n} :", end=' ')
+    if n > 10:
+        print("n > 10")
+    elif 5 <= n <= 10:
+        print("5 <= n <= 10")
+    elif 0 < n < 5:
+        print("0 < n < 5")
+    else:
+        print("n < 0")
