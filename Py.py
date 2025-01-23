9639452d9997d2ef98aab4c9b2589ecbdac1ba85
Hrishikesh Gadekar <55317394+HrishikeshGadekar@users.noreply.github.com>
2025-01-23 11:48:44 +0530

    greatest of three numbers

--- Py.py
+++ Py.py
@@ -3,3 +3,12 @@ if int(input("Enter 1st number: ")) > 10 and int(input("Enter 2nd number: ")) > 
     print('Both > 10')
 else:
     print("Both < 10")
+
+a, b, c = int(input("Enter 1st number: ")), int(input("Enter 2nd number: ")), int(input("Enter 3rd number: "))
+
+if a >= b and a >= c:
+    print(f"a = {a} is the greatest.")
+elif b >= a and b >= c:
+    print(f"b = {b} is the greatest.")
+else:
+    print(f"c = {c} is the greatest.")
